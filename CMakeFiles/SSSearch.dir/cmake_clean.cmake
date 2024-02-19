389fc949845cbbacf0764f2e0047c484596dd0cd
Ryan Smith <rysmith2113@gmail.com>
2024-02-19 16:14:26 -0500

    Initial commit

---
+++ CMakeFiles/SSSearch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/Users/rmsmith/Projects/MathResearchSeminar/TreeSearch/SSSearch"
+  "/Users/rmsmith/Projects/MathResearchSeminar/TreeSearch/SSSearch.pdb"
+  "CMakeFiles/SSSearch.dir/src/main.c.o"
+  "CMakeFiles/SSSearch.dir/src/main.c.o.d"
+  "CMakeFiles/SSSearch.dir/src/treeutils.c.o"
+  "CMakeFiles/SSSearch.dir/src/treeutils.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SSSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
