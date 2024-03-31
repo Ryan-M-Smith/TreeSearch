1911fbb426093f8aee114830d3d69cb6981cda86
Ryan Smith <rysmith2113@gmail.com>
2024-03-31 13:00:15 -0400

    Add a way to determine if a board is a success state for a target card

---
+++ src/successstates.cpp
@@ -0,0 +1,51 @@
+//
+// FILENAME: successstates.cpp | Shifting Stones Search
+// DESCRIPTION: Determine if a given board is a success state for a given target card
+// CREATED: 2024-03-30 @ 7:38 PM
+//
+
+#include "successstates.hpp"
+
+#include <iostream>
+
+namespace success_states {
+	bool isSuccessState(board_t board, const std::string& target) {
+		std::vector<std::string> successStates;
+
+		try {
+			successStates = SUCCESS_STATES.at(target); // The set of possible success states for the given target card
+		}
+		catch (const std::out_of_range&) {
+			return false;
+		}
+
+		bool match = false;
+		std::string boardID = getID(board); // The ID of the current board
+		const char PLACEHOLDERS[] = {'8', '9'};
+
+		for (const auto& state: successStates) {
+			if (match) {
+				return true;
+			}
+
+			match = true;
+
+			//
+			// Check if the board's ID number satisfies any of the success states for the target.
+			//
+			// The digits 8 and 9 are placeholders, and can be filled with any tile on the board.
+			// These values are ignored when comparing.
+			//
+			for (std::size_t i = 0; i < state.size(); i++) {
+				if (state[i] == PLACEHOLDERS[0] || state[i] == PLACEHOLDERS[1] || boardID[i] == state[i]) {
+					continue;
+				}
+
+				match = false;
+				break;
+			}
+		}
+
+		return false;
+	}
+}
